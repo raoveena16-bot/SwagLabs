1a1651d0e22beca0bc3a2cda904b64bf0f0042f5
SeleniumQA <raoveena16@gmail.com>
2026-09-21 13:39:11 -0700

    Migrate AppTest from JUnit to TestNG
    Replaced JUnit test framework with TestNG and added a sample test.

--- SwagLabs/src/test/java/org/SwagLabs/SwagLabs/AppTest.java
+++ SwagLabs/src/test/java/org/SwagLabs/SwagLabs/AppTest.java
@@ -1,38 +1,14 @@
 package org.SwagLabs.SwagLabs;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.testng.annotations.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+
+    @Test
+    public void sampleTest() {
+        System.out.println("TestNG is running");
     }
+}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
     }
 }
